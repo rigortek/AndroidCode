327ceb113f84ee716d105329b9420a44780fccfc
Robin <Robin@sip.sh.cn>
2021-04-14 21:17:58 +0800

    add BlockingQueue test code

--- ideaProjects/JavaLearn/src/com/cw/BlockQueueDemo.java
+++ ideaProjects/JavaLearn/src/com/cw/BlockQueueDemo.java
@@ -1,2 +1,22 @@
-package com.cw;public class BlockQueueDemo {
+package com.cw;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockQueueDemo {
+
+    public static void main(String[] args) {
+        BlockingQueue<String> blockingDeque = new ArrayBlockingQueue<String>(3);
+
+        System.out.println(blockingDeque.add("1"));
+        System.out.println(blockingDeque.add("2"));
+        System.out.println(blockingDeque.add("3"));
+
+        System.out.println(blockingDeque.remove());
+        System.out.println(blockingDeque.remove());
+        System.out.println(blockingDeque.remove());
+        System.out.println(blockingDeque.remove());   // Exception in thread "main" java.util.NoSuchElementException
+
+    }
 }
